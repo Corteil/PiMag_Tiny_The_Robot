0934aac197dddd3f27bd5d8e116d37b6c53acdea
Brian Corteil <brian@corteil.co.uk>
2016-12-11 12:18:11 +0000

    corrected errors in multiplex channel address

--- multiplex.py
+++ multiplex.py
@@ -9,10 +9,14 @@ class multiplex:
 
     def channel(self, address=0x70,channel=0):  # values 0-3 indictae the channel, anything else (eg -1) turns off all channels
         
-        if   (channel==0): action = 0x04
-        elif (channel==1): action = 0x05
-        elif (channel==2): action = 0x06
-        elif (channel==3): action = 0x07
+        if   (channel==0): action = 0x01
+        elif (channel==1): action = 0x02
+        elif (channel==2): action = 0x04
+        elif (channel==3): action = 0x08
+        elif (channel==4): action = 0x10
+        elif (channel==5): action = 0x20
+        elif (channel==6): action = 0x40
+        elif (channel==7): action = 0x80
         else : action = 0x00
 
         self.bus.write_byte_data(address,0x04,action)  #0x04 is the register for switching channels 
